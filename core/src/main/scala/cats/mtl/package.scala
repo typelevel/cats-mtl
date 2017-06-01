a2613b0d0acdfe6e51dee8431c37d6ff40d0278d
Edmund Noble <edmundnoble@gmail.com>
2017-06-01 17:38:40 -0400

    Private the type aliases in the mtl package

--- core/src/main/scala/cats/mtl/package.scala
+++ core/src/main/scala/cats/mtl/package.scala
@@ -4,20 +4,19 @@ import cats.data._
 
 package object mtl {
 
-  type CurryT[T[_[_], _], M[_]] = {type l[A] = T[M, A]}
+  private[mtl] type CurryT[T[_[_], _], M[_]] = {type l[A] = T[M, A]}
 
-  type ReaderTCE[E] = {type l[M[_], A] = ReaderT[M, E, A]}
-  type ReaderTC[M[_], E] = {type l[A] = ReaderT[M, E, A]}
-  type ReaderC[E] = {type l[A] = Reader[E, A]}
-  type OptionTC[M[_]] = {type l[A] = OptionT[M, A]}
-  type StateTC[M[_], S] = {type l[A] = StateT[M, S, A]}
-  type StateTCS[S] = {type l[M[_], A] = StateT[M, S, A]}
-  type EitherC[E] = {type l[A] = E Either A}
-  type EitherTC[M[_], E] = {type l[A] = EitherT[M, E, A]}
-  type EitherTCE[E] = {type l[M[_], A] = EitherT[M, E, A]}
-  type WriterTC[M[_], L] = {type l[A] = WriterT[M, L, A]}
-  type WriterTCL[L] = {type l[M[_], A] = WriterT[M, L, A]}
-  type TupleC[L] = {type l[A] = (L, A)}
-  type of[F[_], G[_]] = {type l[A] = F[G[A]]}
+  private[mtl] type ReaderTCE[E] = {type l[M[_], A] = ReaderT[M, E, A]}
+  private[mtl] type ReaderTC[M[_], E] = {type l[A] = ReaderT[M, E, A]}
+  private[mtl] type ReaderC[E] = {type l[A] = Reader[E, A]}
+  private[mtl] type OptionTC[M[_]] = {type l[A] = OptionT[M, A]}
+  private[mtl] type StateTC[M[_], S] = {type l[A] = StateT[M, S, A]}
+  private[mtl] type StateTCS[S] = {type l[M[_], A] = StateT[M, S, A]}
+  private[mtl] type EitherC[E] = {type l[A] = E Either A}
+  private[mtl] type EitherTC[M[_], E] = {type l[A] = EitherT[M, E, A]}
+  private[mtl] type EitherTCE[E] = {type l[M[_], A] = EitherT[M, E, A]}
+  private[mtl] type WriterTC[M[_], L] = {type l[A] = WriterT[M, L, A]}
+  private[mtl] type WriterTCL[L] = {type l[M[_], A] = WriterT[M, L, A]}
+  private[mtl] type of[F[_], G[_]] = {type l[A] = F[G[A]]}
 
 }
