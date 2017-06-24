d0b0ff94a5a3611a5c507fbb97d3a59c2ddae69c
Edmund Noble <edmundnoble@gmail.com>
2017-06-23 22:45:09 -0400

    TellSyntax

---
+++ core/src/main/scala/cats/mtl/syntax/tell.scala
@@ -0,0 +1,13 @@
+package cats
+package mtl
+package syntax
+
+trait TellSyntax {
+  implicit def toTellOps[L](e: L): TellOps[L] = new TellOps(e)
+}
+
+final class TellOps[L](val e: L) extends AnyVal {
+  def raise[F[_]](implicit applicativeTell: ApplicativeTell[F, L]): F[Unit] = applicativeTell.tell(e)
+}
+
+object tell extends TellSyntax
