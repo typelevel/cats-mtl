a5d9a314386dddaa4e1921180bdb02b86d9601bc
Edmund Noble <edmundnoble@gmail.com>
2017-06-24 00:17:05 -0400

    Remove ScopeOps

--- core/src/main/scala/cats/mtl/syntax/local.scala
+++ core/src/main/scala/cats/mtl/syntax/local.scala
@@ -4,14 +4,10 @@ package syntax
 
 trait LocalSyntax {
   implicit def toLocalOps[F[_], A](fa: F[A]): LocalOps[F, A] = new LocalOps(fa)
-  implicit def toScopeOps[F[_], A](fa: F[A]): ScopeOps[F, A] = new ScopeOps(fa)
 }
 
 final class LocalOps[F[_], A](val fa: F[A]) extends AnyVal {
   def local[E](f: E => E)(implicit applicativeLocal: ApplicativeLocal[F, E]): F[A] = applicativeLocal.local(fa)(f)
-}
-
-final class ScopeOps[F[_], A](val fa: F[A]) extends AnyVal {
   def scope[E](e: E)(implicit applicativeLocal: ApplicativeLocal[F, E]): F[A] = applicativeLocal.scope(fa)(e)
 }
 
