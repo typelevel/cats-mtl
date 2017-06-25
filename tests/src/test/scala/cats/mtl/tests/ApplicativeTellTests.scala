7530f6f751ad8e2fad824c13cae862b96d542b9d
Edmund Noble <edmundnoble@gmail.com>
2017-06-25 04:02:43 -0400

    Forall? More like \forall

--- tests/src/test/scala/cats/mtl/tests/ApplicativeTellTests.scala
+++ tests/src/test/scala/cats/mtl/tests/ApplicativeTellTests.scala
@@ -4,35 +4,36 @@ package tests
 
 import cats.mtl.laws.ApplicativeTellLaws
 import org.scalacheck.{Arbitrary, Cogen, Prop}
-import Prop._
+import Prop.{forAll => ∀}
 import org.typelevel.discipline.Laws
 
 abstract class ApplicativeTellTests[F[_], L] extends Laws {
   def laws: ApplicativeTellLaws[F, L]
 
   def applicativeTell[A: Arbitrary](implicit
-    ArbFA: Arbitrary[F[A]],
-    ArbL: Arbitrary[L],
-    CogenA: Cogen[A],
-    EqFU: Eq[F[Unit]],
-    EqFA: Eq[F[A]]
-  ): RuleSet = {
+                                    ArbFA: Arbitrary[F[A]],
+                                    ArbL: Arbitrary[L],
+                                    CogenA: Cogen[A],
+                                    EqFU: Eq[F[Unit]],
+                                    EqFA: Eq[F[A]]
+                                   ): RuleSet = {
     new DefaultRuleSet(
       name = "applicativeTell",
       parent = None,
-      "tell forms semigroup action" -> forAll(laws.tellTwiceIsTellCombined _),
+      "tell forms semigroup action" -> ∀(laws.tellTwiceIsTellCombined _),
       "tell forms monoid action" -> (laws.tellEmptyIsPureUnit: Prop),
-      "tuple is (flip writer)" -> forAll(laws.tupleIsWriterFlipped[A] _),
-      "writer is tell and map" -> forAll(laws.writerIsTellAndMap[A] _)
+      "tuple is (flip writer)" -> ∀(laws.tupleIsWriterFlipped[A] _),
+      "writer is tell ann map" -> ∀(laws.writerIsTellAndMap[A] _)
     )
   }
 
 }
 
 object ApplicativeTellTests {
-  def apply[F[_], L](implicit tell: ApplicativeTell[F, L]): ApplicativeTellTests[F, L] =
+  def apply[F[_], L](implicit tell: ApplicativeTell[F, L]): ApplicativeTellTests[F, L] = {
     new ApplicativeTellTests[F, L] {
       def laws: ApplicativeTellLaws[F, L] = ApplicativeTellLaws[F, L]
     }
+  }
 }
 
