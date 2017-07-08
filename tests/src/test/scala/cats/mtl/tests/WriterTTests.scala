14fa48d94bba5802020b2b1ee1f76aac5243b252
Edmund Noble <edmundnoble@gmail.com>
2017-07-08 19:52:01 -0400

    Scalastyle and instance scoping in tests

--- tests/src/test/scala/cats/mtl/tests/WriterTTests.scala
+++ tests/src/test/scala/cats/mtl/tests/WriterTTests.scala
@@ -7,33 +7,37 @@ import cats.arrow.FunctionK
 import cats.data._
 import cats.instances.all._
 import cats.laws.discipline.SerializableTests
-import cats.mtl.instances.all._
+import cats.mtl.instances.listen._
 import cats.laws.discipline.arbitrary._
 import cats.mtl.laws.discipline._
 import org.scalacheck._
 
 class WriterTTests extends BaseSuite {
-  checkAll("WriterT[Option, String, String]",
-    FunctorListenTests[WriterTC[Option, String]#l, String].functorListen[String, String])
-  checkAll("FunctorListen[WriterT[Option, String, ?]]",
-    SerializableTests.serializable(FunctorListen[WriterTC[Option, String]#l, String]))
-
   implicit val arbFunctionK: Arbitrary[Option ~> Option] =
     Arbitrary(Gen.oneOf(new (Option ~> Option) {
       def apply[A](fa: Option[A]): Option[A] = None
     }, FunctionK.id[Option]))
 
+  checkAll("WriterT[Option, String, String]",
+    FunctorListenTests[WriterTC[Option, String]#l, String].functorListen[String, String])
+  checkAll("FunctorListen[WriterT[Option, String, ?]]",
+    SerializableTests.serializable(FunctorListen[WriterTC[Option, String]#l, String]))
+
   {
+    implicit val applicativeLayerFunctor: ApplicativeLayerFunctor[WriterTC[Option, String]#l, Option] =
+      cats.mtl.instances.writert.writerApplicativeLayerFunctor[Option, String]
     checkAll("WriterT[Option, String, ?]",
       ApplicativeLayerFunctorTests[WriterTC[Option, String]#l, Option].applicativeLayerFunctor[Boolean, Boolean])
     checkAll("ApplicativeLayerFunctor[WriterT[Option, String, ?], Option]",
-      SerializableTests.serializable(ApplicativeLayerFunctor[WriterTC[Option, String]#l, Option]))
+    SerializableTests.serializable(applicativeLayerFunctor))
   }
 
   {
+    implicit val functorLayerFunctor: FunctorLayerFunctor[WriterTC[Option, String]#l, Option] =
+      cats.mtl.instances.writert.writerFunctorLayerFunctor[Option, String]
     checkAll("WriterT[Option, String, ?]",
       FunctorLayerFunctorTests[WriterTC[Option, String]#l, Option].functorLayerFunctor[Boolean])
     checkAll("FunctorLayerFunctor[WriterT[Option, String, ?], Option]",
-      SerializableTests.serializable(FunctorLayerFunctor[WriterTC[Option, String]#l, Option]))
+      SerializableTests.serializable(functorLayerFunctor))
   }
 }
