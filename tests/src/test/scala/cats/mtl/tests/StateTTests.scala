a100d0e2460204120ebb25a25437c4b6b0c7f777
Edmund Noble <edmundnoble@gmail.com>
2017-07-22 23:11:05 -0400

    Add OptionT, EitherT, ReaderT, WriterT all over StateT MonadState tests

--- tests/src/test/scala/cats/mtl/tests/StateTTests.scala
+++ tests/src/test/scala/cats/mtl/tests/StateTTests.scala
@@ -13,8 +13,12 @@ import org.scalacheck.{Arbitrary, Gen}
 import cats.mtl.instances.state._
 import cats.mtl.instances.local._
 import cats.mtl.instances.readert._
+import cats.mtl.instances.writert._
+import cats.mtl.instances.optiont._
+import cats.mtl.instances.eithert._
 import cats.mtl.hierarchy.BaseHierarchy._
 import cats.instances.all._
+import cats.mtl.MonadLayerControl.Aux
 
 class StateTTests extends BaseSuite {
   implicit val arbFunctionK: Arbitrary[Option ~> Option] =
@@ -69,22 +73,83 @@ class StateTTests extends BaseSuite {
   checkAll("ApplicativeAsk[StateT[Option, String, ?]]",
     SerializableTests.serializable(ApplicativeAsk[StateTC[Option, String]#l, String]))
 
-  checkAll("ReaderT[StateT[Option, String, ?], Int, String]",
-    MonadStateTests[ReaderTIntOverStateTStringOverOption, String]
+  locally {
+    implicit def slowCatsLawsEqForFn1[A, B](implicit A: Arbitrary[A], B: Eq[B]): Eq[A => B] =
+      tweakableCatsLawsEqForFn1[A, B](20)
+
+    checkAll("ReaderT[StateT[Option, String, ?], Int, String]",
+      MonadStateTests[ReaderTIntOverStateTStringOverOption, String]
+        .monadState[String])
+    checkAll("MonadState[ReaderT[StateT[Option, String, ?], Int, ?]]",
+      SerializableTests.serializable(MonadState[ReaderTIntOverStateTStringOverOption, String]))
+
+    checkAll("ReaderT[StateT[Option, String, ?], Int, String]",
+      FunctorTellTests[ReaderTIntOverStateTStringOverOption, String]
+        .functorTell[String])
+    checkAll("FunctorTell[ReaderT[StateT[Option, String, ?], Int, ?]]",
+      SerializableTests.serializable(FunctorTell[ReaderTIntOverStateTStringOverOption, String]))
+
+    checkAll("ReaderT[StateT[Option, String, ?], Int, String]",
+      ApplicativeAskTests[ReaderTIntOverStateTStringOverOption, String]
+        .applicativeAsk[String])
+    checkAll("ApplicativeAsk[ReaderT[StateT[Option, String, ?], Int, ?]]",
+      SerializableTests.serializable(ApplicativeAsk[ReaderTIntOverStateTStringOverOption, String]))
+  }
+
+  checkAll("WriterT[StateT[Option, String, ?], Int, String]",
+    MonadStateTests[WriterTIntOverStateTStringOverOption, String]
       .monadState[String])
-  checkAll("MonadState[ReaderT[StateT[Option, String, ?], Int, ?]]",
-    SerializableTests.serializable(MonadState[StateTC[Option, String]#l, String]))
+  checkAll("MonadState[WriterT[StateT[Option, String, ?], Int, ?]]",
+    SerializableTests.serializable(MonadState[WriterTIntOverStateTStringOverOption, String]))
 
-  checkAll("ReaderT[StateT[Option, String, ?], Int, String]",
-    FunctorTellTests[ReaderTIntOverStateTStringOverOption, String]
+  checkAll("WriterT[StateT[Option, String, ?], Int, String]",
+    FunctorTellTests[WriterTIntOverStateTStringOverOption, String]
       .functorTell[String])
-  checkAll("FunctorTell[ReaderT[StateT[Option, String, ?], Int, ?]]",
-    SerializableTests.serializable(FunctorTell[ReaderTIntOverStateTStringOverOption, String]))
+  checkAll("FunctorTell[WriterT[StateT[Option, String, ?], Int, ?]]",
+    SerializableTests.serializable(FunctorTell[WriterTIntOverStateTStringOverOption, String]))
 
-  checkAll("ReaderT[StateT[Option, String, ?], Int, String]",
-    ApplicativeAskTests[ReaderTIntOverStateTStringOverOption, String]
+  checkAll("WriterT[StateT[Option, String, ?], Int, String]",
+    ApplicativeAskTests[WriterTIntOverStateTStringOverOption, String]
       .applicativeAsk[String])
-  checkAll("ApplicativeAsk[ReaderT[StateT[Option, String, ?], Int, ?]]",
-    SerializableTests.serializable(ApplicativeAsk[ReaderTIntOverStateTStringOverOption, String]))
+  checkAll("ApplicativeAsk[WriterT[StateT[Option, String, ?], Int, ?]]",
+    SerializableTests.serializable(ApplicativeAsk[WriterTIntOverStateTStringOverOption, String]))
+
+  // TODO: can't test StateT nested with StateT for now for some reason, investigate later
+
+  checkAll("EitherT[StateT[Option, String, ?], Int, String]",
+    MonadStateTests[EitherTIntOverStateTStringOverOption, String]
+      .monadState[String])
+  checkAll("MonadState[EitherT[StateT[Option, String, ?], Int, ?]]",
+    SerializableTests.serializable(MonadState[EitherTIntOverStateTStringOverOption, String]))
+
+  checkAll("EitherT[StateT[Option, String, ?], Int, String]",
+    FunctorTellTests[EitherTIntOverStateTStringOverOption, String]
+      .functorTell[String])
+  checkAll("FunctorTell[EitherT[StateT[Option, String, ?], Int, ?]]",
+    SerializableTests.serializable(FunctorTell[EitherTIntOverStateTStringOverOption, String]))
+
+  checkAll("EitherT[StateT[Option, String, ?], Int, String]",
+    ApplicativeAskTests[EitherTIntOverStateTStringOverOption, String]
+      .applicativeAsk[String])
+  checkAll("ApplicativeAsk[EitherT[StateT[Option, String, ?], Int, ?]]",
+    SerializableTests.serializable(ApplicativeAsk[EitherTIntOverStateTStringOverOption, String]))
+
+  checkAll("OptionT[StateT[Option, String, ?], Int, String]",
+    MonadStateTests[OptionTOverStateTStringOverOption, String]
+      .monadState[String])
+  checkAll("MonadState[OptionT[StateT[Option, String, ?], Int, ?]]",
+    SerializableTests.serializable(MonadState[OptionTOverStateTStringOverOption, String]))
+
+  checkAll("OptionT[StateT[Option, String, ?], Int, String]",
+    FunctorTellTests[OptionTOverStateTStringOverOption, String]
+      .functorTell[String])
+  checkAll("FunctorTell[OptionT[StateT[Option, String, ?], Int, ?]]",
+    SerializableTests.serializable(FunctorTell[OptionTOverStateTStringOverOption, String]))
+
+  checkAll("OptionT[StateT[Option, String, ?], Int, String]",
+    ApplicativeAskTests[OptionTOverStateTStringOverOption, String]
+      .applicativeAsk[String])
+  checkAll("ApplicativeAsk[OptionT[StateT[Option, String, ?], Int, ?]]",
+    SerializableTests.serializable(ApplicativeAsk[OptionTOverStateTStringOverOption, String]))
 
 }
