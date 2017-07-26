3c0e949642bfa526fc952d66771553d9b819e476
Edmund Noble <edmundnoble@gmail.com>
2017-07-25 21:07:59 -0400

    More syntax coverage

--- tests/src/test/scala/cats/mtl/tests/Syntax.scala
+++ tests/src/test/scala/cats/mtl/tests/Syntax.scala
@@ -12,20 +12,30 @@ final class Syntax extends BaseSuite {
     import cats.data._
     test("ApplicativeAsk") {
       ((i: Int) => "$" + i.toString).reader[ReaderIntId]
+      val asked = ApplicativeAsk.ask[ReaderIntId, Int].run(1)
+      val askedE = ApplicativeAsk.askE[Int]().run(1)
+      val askedF = ApplicativeAsk.askF[ReaderIntId]().run(1)
+      val readerFE = ApplicativeAsk.readerFE[ReaderIntId, Int](i => "$" + i.toString).run(1)
     }
     test("FunctorListen") {
       val lift = WriterT.lift[Option, String, Int](Option.empty[Int])
       val listen: WriterT[Option, String, (Int, String)] = lift.listen
       val listens: WriterT[Option, String, (Int, String)] = lift.listens((_: String) + "suffix")
+      val listenC = FunctorListen.listen(lift)
+      val listensC = FunctorListen.listens(lift)((_: String) + "suffix")
     }
     test("ApplicativeLocal") {
       val fa: OptionT[FunctionC[String]#l, Int] = OptionT.pure(1)
-      val y = fa.local[String](s => s + "!")
-      val z = fa.scope[String]("state")
+      val y = fa.local[String](s => s + "!").value("ha")
+      val z = fa.scope[String]("state").value("ha")
+      ApplicativeLocal.local((s: String) => s + "!")(ApplicativeAsk.askF[FunctionC[String]#l]()).apply("ha")
     }
     test("FunctorRaise") {
       val fa: Either[String, Int] = "ha".raise[EitherC[String]#l, Int]
       val fat: EitherT[Option, String, Int] = "ha".raise[EitherTC[Option, String]#l, Int]
+      val faC = FunctorRaise.raise[EitherC[String]#l, String, Int]("ha")
+      val fatC = FunctorRaise.raiseE[String]("ha")
+      val faeC = FunctorRaise.raiseF[EitherC[String]#l]("ha")
     }
     test("FunctorEmpty") {
       def operateFunctorEmpty[F[_]: FunctorEmpty](fi: F[Int]): Unit = {
