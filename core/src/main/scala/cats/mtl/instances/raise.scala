e5050ecac68b88e32db6fb32e0bff9751576be25
Luka Jacobowitz <luka.jacobowitz@fh-duesseldorf.de>
2018-08-14 11:05:25 +0200

    Add an ApplicativeHandle type class  (#41)
    *  Add an ApplicativeHandle typeclass
    
    * Add laws and tests
    
    * Extra tests

--- core/src/main/scala/cats/mtl/instances/raise.scala
+++ core/src/main/scala/cats/mtl/instances/raise.scala
@@ -2,8 +2,8 @@ package cats
 package mtl
 package instances
 
-import cats.data.EitherT
-import cats.mtl.FunctorRaise
+
+import cats.data.{EitherT, OptionT, Validated}
 import cats.mtl.lifting.FunctorLayer
 
 trait RaiseInstances extends RaiseLowPriorityInstances1 {
@@ -20,7 +20,7 @@ trait RaiseInstances extends RaiseLowPriorityInstances1 {
 }
 
 private[instances] trait RaiseLowPriorityInstances1 {
-  implicit final def raiseEitherT[M[_], E](implicit M: Applicative[M]): FunctorRaise[EitherTC[M, E]#l, E] = {
+  final def raiseEitherT[M[_], E](implicit M: Applicative[M]): FunctorRaise[EitherTC[M, E]#l, E] = {
     new FunctorRaise[EitherTC[M, E]#l, E] {
       val functor: Functor[EitherTC[M, E]#l] = EitherT.catsDataFunctorForEitherT(M)
 
@@ -28,13 +28,35 @@ private[instances] trait RaiseLowPriorityInstances1 {
     }
   }
 
-  implicit final def raiseEither[E]: FunctorRaise[EitherC[E]#l, E] = {
+  final def raiseOptionT[M[_]](implicit M: Applicative[M]): FunctorRaise[OptionTC[M]#l, Unit] = {
+    new FunctorRaise[OptionTC[M]#l, Unit] {
+      val functor: Functor[OptionTC[M]#l] = OptionT.catsDataFunctorForOptionT(M)
+
+      def raise[A](e: Unit): OptionT[M, A] = OptionT(M.pure(None))
+    }
+  }
+
+  final def raiseEither[E]: FunctorRaise[EitherC[E]#l, E] = {
     new FunctorRaise[EitherC[E]#l, E] {
       val functor: Functor[EitherC[E]#l] = cats.instances.either.catsStdInstancesForEither
 
       def raise[A](e: E): Either[E, A] = Left(e)
     }
   }
+
+  final val raiseOption: FunctorRaise[Option, Unit] =
+    new FunctorRaise[Option, Unit] {
+      val functor: Functor[Option] = cats.instances.option.catsStdInstancesForOption
+
+      def raise[A](e: Unit): Option[A] = None
+    }
+
+  final def raiseValidated[E](implicit E: Semigroup[E]): FunctorRaise[Validated[E, ?], E] =
+    new FunctorRaise[Validated[E, ?], E] {
+      val functor: Functor[Validated[E, ?]] = Validated.catsDataApplicativeErrorForValidated[E]
+
+      def raise[A](e: E): Validated[E, A] = Validated.Invalid(e)
+    }
 
   // TODO: an instance derived from another FunctorRaise instance and an Inject instance ("subtyping")
 }
