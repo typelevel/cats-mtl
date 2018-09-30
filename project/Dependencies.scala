2978749daef2d233fe521bb323182b5e861d6a22
Scala steward <43047562+scala-steward@users.noreply.github.com>
2018-09-30 20:52:38 +0200

    Update machinist to 0.6.5 (#79)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val macroParadise = "2.1.1"
     val kindProjector = "0.9.6"
     val simulacrum = "0.13.0"
-    val machinist = "0.6.4"
+    val machinist = "0.6.5"
     val cats = "1.4.0"
     val shapeless = "2.3.3"
   }
