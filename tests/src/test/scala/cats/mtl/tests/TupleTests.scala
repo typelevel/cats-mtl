d43573d4c67ccdbc607b7362c663b3940f877fc5
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2018-10-04 18:42:52 +0200

    More tests

--- tests/src/test/scala/cats/mtl/tests/TupleTests.scala
+++ tests/src/test/scala/cats/mtl/tests/TupleTests.scala
@@ -8,13 +8,13 @@ import cats.instances.all._
 import cats.laws.discipline.SerializableTests
 import cats.mtl.instances.all._
 import cats.laws.discipline.arbitrary._
-import cats.mtl.laws.discipline.FunctorListenTests
+import cats.mtl.laws.discipline.ApplicativeCensorTests
 
 class TupleTests extends BaseSuite {
-  checkAll("FunctorListen[(String, ?), String]",
-    FunctorListenTests[TupleC[String]#l, String]
-      .functorListen[String, String])
-  checkAll("FunctorListen[(String, ?), String]",
-    SerializableTests.serializable(FunctorListen[TupleC[String]#l, String]))
+  checkAll("ApplicativeCensor[(String, ?), String]",
+    ApplicativeCensorTests[TupleC[String]#l, String]
+      .applicativeCensor[String, String])
+  checkAll("ApplicativeCensor[(String, ?), String]",
+    SerializableTests.serializable(ApplicativeCensor[TupleC[String]#l, String]))
 }
 
