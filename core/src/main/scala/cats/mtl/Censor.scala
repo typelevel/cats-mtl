44f1acaec667a5e2bb63a93cefb9de9b305c36fa
Daniel Spiewak <djspiewak@gmail.com>
2020-06-01 09:18:49 -0600

    Forgot to annotation Censor

--- core/src/main/scala/cats/mtl/Censor.scala
+++ core/src/main/scala/cats/mtl/Censor.scala
@@ -30,6 +30,10 @@ import cats.data.{
 }
 import cats.implicits._
 
+import scala.annotation.implicitNotFound
+
+@implicitNotFound(
+  "Could not find an implicit instance of Censor[${F}, ${L}]. If you wish\nto capture side-channel output of type ${L} at this location, you may want\nto construct a value of type WriterT for this call-site, rather than ${F}.\nAn example type:\n\n  WriterT[${F}, ${L}, *]\n\nOne use-case for this would be if ${L} represents an accumulation of values\nwhich are produced by this function *in addition to* its normal results.\nThis can be used to implement some forms of pure logging.\n\nIf you do not wish to capture a side-channel of type ${L} at this location,\nyou should add an implicit parameter of this type to your function. For\nexample:\n\n  (implicit fcensor: Censor[${F}, ${L}}])\n")
 trait Censor[F[_], L] extends Listen[F, L] {
   val applicative: Applicative[F]
   val monoid: Monoid[L]
