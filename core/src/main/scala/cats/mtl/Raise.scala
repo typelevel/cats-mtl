ac84ac570384b7bd402bc2aba17a3cb52a8d9044
Rasmus Précenth <rasmus@precenth.eu>
2025-08-21 07:06:45 +0000

    mention Handle.allowF in Raise's implicitNotFound message

--- core/src/main/scala/cats/mtl/Raise.scala
+++ core/src/main/scala/cats/mtl/Raise.scala
@@ -62,7 +62,7 @@ import scala.util.control.NonFatal
  * }}}
  */
 @implicitNotFound(
-  "Could not find an implicit instance of Raise[${F}, ${E}]. If you have\na good way of handling errors of type ${E} at this location, you may want\nto construct a value of type EitherT for this call-site, rather than ${F}.\nAn example type:\n\n  EitherT[${F}, ${E}, *]\n\nThis is analogous to writing try/catch around this call. The EitherT will\n\"catch\" the errors of type ${E}.\n\nIf you do not wish to handle errors of type ${E} at this location, you should\nadd an implicit parameter of this type to your function. For example:\n\n  (implicit fraise: Raise[${F}, ${E}])\n")
+  "Could not find an implicit instance of Raise[${F}, ${E}]. If you have a\ngood way of handling errors of type ${E} at this location, you may want\nto wrap the call-site in Handle.allowF, like so:\n\n  Handle.allowF[${F}, ${E}] { implicit h =>\n    // call code requiring Raise[${F}, ${E}]\n  }.rescue { e: ${E} =>\n    // handle the error\n  }\n\nThis is analogous to writing try/catch around this call ensuring that\nall errors of type ${E} are handled in the rescue block.\n\nAlternatively, you may want to construct a value of type EitherT for\nthis call-site, rather than ${F}.\nAn example type:\n\n  EitherT[${F}, ${E}, *]\n\nThe EitherT will \"catch\" the errors of type ${E}.\n\nIf you do not wish to handle errors of type ${E} at this location, you should\nadd an implicit parameter of this type to your function. For example:\n\n  (implicit fraise: Raise[${F}, ${E}])\n")
 trait Raise[F[_], -E] extends Serializable {
   def functor: Functor[F]
 
